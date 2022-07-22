00ae1b4a4b939ea773564c136547e1aa7b286d16
hosang6 <109564217+hosang6@users.noreply.github.com>
2022-07-22 22:33:00 +0900

    express create read commit

--- myapp/src/app.ts
+++ myapp/src/app.ts
@@ -3,29 +3,78 @@ import {Cat,CatType} from './app.model';
 
 const app: express.Express = express();
 
+//* logging middelware
 app.use((req, res, next) => {
     console.log(req.rawHeaders[1]);
     console.log('this is logging middelware');
     next();
 });
 
-app.get('/cats/som', (req, res, next) =>{
-    console.log('this is som middelware');
-    next();
+//* json middleware
+app.use(express.json());
+
+
+//* READ 고양이 전체 데이터 다 조회
+app.get('/cats', (req, res)=>{
+    try {
+        const cats = Cat;
+       // throw new Error("db connect error");
+        res.status(200).send({
+            success: true,
+            data:{
+                cats,
+            },
+        });
+    } catch (error: any) {
+        res.status(400).send({
+            success: false,
+            error: error.message,
+        });
+    }
 });
 
-app.get('/', (req: express.Request, res: express.Response) =>{
-    res.send({ cats: Cat });
+//* READ 특정 고양이 데이터 조회
+app.get('/cats/:id', (req, res)=>{
+    try {
+        const params = req.params;
+        console.log(params);
+        const cats = Cat.find((cat)=>{
+            return cat.id === params.id;
+        })
+        res.status(200).send({
+            success: true,
+            data:{
+                cats,
+            },
+        });
+    } catch (error: any) {
+        res.status(400).send({
+            success: false,
+            error: error.message,
+        });
+    }
 });
 
-app.get('/cats/blue',(req,res) => {
-    res.send({cats: Cat, blue: Cat[0]});
+//* CREATE 새로운 고양이 추가 API
+app.post('/cats', (req, res)=>{
+    try{
+        const data = req.body;
+        console.log(data);
+        Cat.push(data); // create
+        res.status(200).send({
+            success: true,
+            data: { data },
+        });
+    }catch (error: any) {
+        res.status(400).send({
+            success: false,
+            error: error.message,
+        });
+    }
 });
 
-app.get('/cats/som',(req,res) => {
-    res.send({ som: Cat[1]});
-});
 
+/* 404 middle ware */
 app.use((req,res,next)=>{
     console.log('this is error middelware');
     res.send({error: '404 not found error'});
@@ -34,3 +83,7 @@ app.use((req,res,next)=>{
 
 app.listen(3000, ()=>{console.log('server is on...');
 });
+
+
+
+
